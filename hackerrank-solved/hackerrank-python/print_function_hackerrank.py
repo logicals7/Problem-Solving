e9b7077f096c52e13d9e5ba1289b209d58751054
Krishan Kant <42918717+logicals7@users.noreply.github.com>
2021-01-26 21:34:26 +0530

    print function hackerrank python solutions

---
+++ hackerrank-solved/hackerrank-python/print_function_hackerrank.py
@@ -0,0 +1,11 @@
+# Problem Statement: https://www.hackerrank.com/challenges/python-print/problem
+
+if __name__ == '__main__':
+        n = int(input())
+
+        def printx():
+
+            for i in range(1,n+1):
+                print(i,end='')
+
+        printx()
