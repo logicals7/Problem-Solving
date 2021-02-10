87d93474dfe57f1d588796fc239708c4add922a3
Krishan Kant <42918717+logicals7@users.noreply.github.com>
2021-02-10 09:52:42 +0530

    stringstream hackerrank c++ solution

---
+++ hackerrank-solved/hackerrank-cpp/Stringstream_hackerrank.cpp
@@ -0,0 +1,31 @@
+//Problem Statement: https://www.hackerrank.com/challenges/c-tutorial-stringstream/problem
+
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> parseInts(string str) {
+	// Complete this function
+    stringstream ss(str);
+    vector<int> vec1;
+    int temp;
+    char ch;
+    
+    while(ss>>temp){
+        vec1.push_back(temp);
+        ss>>ch;
+    }
+    return vec1;
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    
+    return 0;
+}
